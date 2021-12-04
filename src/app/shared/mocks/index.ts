9f65049227a7132aa85e5ef180b796e9494cacd7
Mauricio Mino <mauriminio96@gmail.com>
2021-12-03 21:33:54 -0300

    feat: end implementation and unit-testing coverage

---
+++ src/app/shared/mocks/index.ts
@@ -0,0 +1,1 @@
+export * from './user-entity-manager-mock-factory';
---
+++ src/app/shared/mocks/user-entity-manager-mock-factory.ts
@@ -0,0 +1,15 @@
+import {of} from "rxjs";
+
+import {ListResponse, UserModel} from "../../data-layer";
+
+export class UserEntityManagerMockFactory {
+  static get defaultMock(): any {
+    const mockService = jasmine.createSpyObj('UserEntityManager',['getUsers']);
+    mockService.getUsers.and.returnValue(of({
+      totalCount: 1,
+      items: [{avatarUrl: 'test', login: 'test', type: 'user'} as UserModel]
+    } as ListResponse<UserModel>));
+
+    return mockService;
+  }
+}
